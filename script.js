eb8d1249a010bfd72805942bd3f5adc21e4440f4
Gulnar <gulnar.amiraslanova@gmail.com>
2023-03-25 22:03:30 +0900

    ContactForm

--- script.js
+++ script.js
@@ -9,3 +9,28 @@ window.onscroll = () =>{
     menu.classList.remove('fa-times');
     header.classList.remove('active', target=== header);
 }
+
+
+function sendMail(){
+    let params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value,
+        phone: document.getElementById("phone").value
+    };
+    const serviceID = "service_1r9lehn";
+    const templateID = "template_qb5sx2q";
+
+
+    emailjs
+    .send(serviceID,templateID,params)
+    .then(
+        res => {
+            document.getElementById("name").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("message").value = "";
+            document.getElementById("phone").value = "";
+            console.log(res);
+            alert("Message sent successfully");
+        });
+}
