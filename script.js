05650d11ba3b622f7c7be5a11a641e70bd7c0f1d
Gulnar <gulnar.amiraslanova@gmail.com>
2023-03-25 22:33:43 +0900

    formCatch

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ window.onscroll = () =>{
 
 
 function sendMail(){
+  
     let params = {
         name: document.getElementById("name").value,
         email: document.getElementById("email").value,
@@ -21,16 +22,17 @@ function sendMail(){
     const serviceID = "service_1r9lehn";
     const templateID = "template_qb5sx2q";
 
-
+    
     emailjs
-    .send(serviceID,templateID,params)
-    .then(
-        res => {
+        .send(serviceID,templateID,params)
+        .then((res) => {
             document.getElementById("name").value = "";
             document.getElementById("email").value = "";
             document.getElementById("message").value = "";
             document.getElementById("phone").value = "";
             console.log(res);
             alert("Message sent successfully");
-        });
+        })
+        .catch((error)=>{
+            console.log('Oops... ' + error)});
 }
